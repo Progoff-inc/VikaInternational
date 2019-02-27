5f89b106d6a2b3320a41f0b95deb3ff51f18b421
Иван Номоконов <171035@edu.fa.ru>
2019-02-27 16:41:44 +0300

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+  sections:Section[] = [
+    {
+      Name:"Пинцеты для наращивания ресниц",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 2",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 3",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 4",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 5",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 6",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 7",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 8",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 9",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 10",
+      Image:"../../assets/images/goods/44.jpg"
+    },
+    {
+      Name:"Раздел 11",
+      Image:"../../assets/images/goods/44.jpg"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
+  getRow(i){
+    console.log((this.sections.length-this.sections.length%i)/i);
+    return Math.round((this.sections.length-this.sections.length%i)/i);
+  }
+}
 
+export interface Section{
+  Name:string;
+  Image:string;
 }
